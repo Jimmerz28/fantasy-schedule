b1c57f837fa73b44b831995225de67dbcfc26ae3
James Daniel Holby <jamesdholby@gmail.com>
2018-08-14 01:13:19 +0200

    Filter only the favorites

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -1,33 +1,45 @@
 import React from 'react';
 
-const EventList = ({ events, filters, onFav }) => {
+const EventList = ({ events, filters, onFav, favs }) => {
 
     const list = events.map(({ day, events }) => {
 
         const daysEvents = events
+
             .filter(event => {
                 // First check to see if we even have anything to filter
                 if (filters.tags.length === 0) {
+
                     // If we don't just show all events
-                    return event;
+                    return true;
                 }
 
                 // Otherwise check if our event type should be filtered out
                 return filters.tags.includes(event["Event Type"]);
             })
 
-            // @TODO: Filter out only favs
+            .filter(event => {
+
+                if (filters.onlyFavs) {
+                    return favs.includes(event["Game ID"]);
+                }
+
+                return true;
+            })
 
             .map(event => {
 
                 const id = event["Game ID"];
+                const checked = favs.includes(id);
 
-                return <li key={id}>
+                return (
+                    <li key={id}>
                         <p>{event["Title"]}</p>
                         <p>{event["Event Type"]}</p>
                         <label htmlFor={id}>Favorite</label>
-                        <input type="checkbox" name={id} id={id} onChange={onFav} />
-                    </li>;
+                        <input type="checkbox" name={id} id={id} onChange={onFav} checked={checked} />
+                    </li>
+                );
             });
 
         return (
