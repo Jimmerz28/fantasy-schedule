be6d6be21abb96c9d2176cd6d14ee38afdae6c80
James Daniel Holby <jamesdholby@gmail.com>
2018-09-01 18:16:20 +0200

    Slim down redux middleware for production

--- src/Store.js
+++ src/Store.js
@@ -1,17 +1,23 @@
-import { applyMiddleware, createStore } from "redux";
+import { applyMiddleware, compose, createStore } from "redux";
+
 import { createLogger } from "redux-logger";
+import genconApp from "./Reducers";
 import thunkMiddleware from "redux-thunk";
 
-import genconApp from "./Reducers";
-
 const loggerMiddleware = createLogger();
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+let enhancer = composeEnhancers(applyMiddleware(
+    thunkMiddleware,
+    loggerMiddleware
+));
+
+if (process.env.NODE_ENV === 'production') {
+    enhancer = applyMiddleware(thunkMiddleware);
+}
 
 const store = createStore(
     genconApp,
-    applyMiddleware(
-        thunkMiddleware,
-        loggerMiddleware
-    )
+    enhancer
 );
 
 export default store;
