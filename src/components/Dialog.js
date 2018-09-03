d8be37a18cf39d998cff260c28c79d03db4c6844
James Daniel Holby <jamesdholby@gmail.com>
2018-09-03 10:02:16 +0200

    Keep dialog fixed to the center of the screen

--- src/components/Dialog.js
+++ src/components/Dialog.js
@@ -1,14 +1,13 @@
 // @flow
 
 import React from 'react';
-
 import type { VanillaEvent } from '../types';
 import styles from './Dialog.module.css';
 import EventDetail from './EventDetail';
 
 const Dialog = ({ show, event, onClose }: { show: boolean, event: VanillaEvent, onClose: Function }) => {
     return (
-        <dialog className={styles["event-dialog"]}>
+        <dialog className={ styles["event-dialog"] }>
             <section>
                 <button type="button" onClick={onClose}>Close</button>
                 <EventDetail event={event} />
