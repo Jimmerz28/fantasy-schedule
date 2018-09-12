714808d04c14fead24d4d54010ad84b2bd5bc356
James Daniel Holby <jamesdholby@gmail.com>
2018-09-13 00:35:14 +0200

    Add time to each event and style it

--- src/components/Event.js
+++ src/components/Event.js
@@ -10,11 +10,13 @@ const Event = ({ event, onClick, onFav, isFav }) => {
     return (
         <li className={styles.event} onClick={onClick.bind(this, id)}>
             <p>{eventStartTime(event)}</p>
-            <p>{event["Title"]}</p>
-            <p>{event["Event Type"]}</p>
-            <label htmlFor={id}>Favorite</label>
-            <input type="checkbox" name={id} id={id}
-                onChange={onFav} checked={isFav} />
+            <div>
+                <p>{event["Title"]}</p>
+                <p>{event["Event Type"].slice(6)}</p>
+                <label htmlFor={id}>Favorite</label>
+                <input type="checkbox" name={id} id={id}
+                    onChange={onFav} checked={isFav} />
+            </div>
         </li>
     );
 }
