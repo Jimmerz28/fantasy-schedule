b23d6bf7721e35318ccf6794515d4106399eff87
James Daniel Holby <jamesdholby@gmail.com>
2018-09-22 13:48:07 +0200

    Create React component template

---
+++ battlecry/generators/component/templates/__NaMe__.js
@@ -0,0 +1,20 @@
+// @flow
+import React, { Component } from "react";
+
+type Props = {
+
+};
+
+type State = {
+
+};
+
+class __NaMe__ extends Component<Props, State> {
+    render() {
+        return (
+            <div>__NaMe__ Works!</div>
+        );
+    }
+}
+
+export default __NaMe__;
--- battlecry/generators/scomponent/templates/__NaMe__.js
+++ battlecry/generators/scomponent/templates/__NaMe__.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React from "react";
 
 const __NaMe__ = () => {
     return (
-        <div>__NaMe__</div>
+        <div>__NaMe__ Works!</div>
     );
 }
 
