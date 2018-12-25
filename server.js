b4f9f67d9eff1a304c956e258ca7065e3c4bf014
James Daniel Holby <jamesdholby@gmail.com>
2018-12-26 00:41:25 +0100

    Add nodejs server and gzip mockfiles

--- server.js
+++ server.js
@@ -1,27 +1,44 @@
-import express from "express";
 import fs from "fs";
 import { join } from "path";
 
+import cors from "cors";
+import express from "express";
+
 let app = express();
 
-app.get("/events", (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
+function stopServer() {
+    server.close(() => {
+        process.exit(0);
+        console.info("Server stopping...")
+    });
+}
 
-    fs.readFile(join(`${__dirname}/mocks/events.json`), (err, data) => {
+app.use(cors());
+
+app.get("/events", (req, res) => {
+    res.setHeader("Content-Type", "application/json");
+    res.setHeader("Content-Encoding", "gzip");
+
+    fs.readFile(join(`${__dirname}/mocks/events.json.gz`), (err, data) => {
         if (err) throw err;
         res.end(data);
     });
 });
 
 app.get("/colors", (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
+    res.setHeader("Content-Type", "application/json");
+    res.setHeader("Content-Encoding", "gzip");
 
-    fs.readFile(join(`${__dirname}/mocks/colors.json`), (err, data) => {
+    fs.readFile(join(`${__dirname}/mocks/colors.json.gz`), (err, data) => {
         if (err) throw err;
         res.end(data);
     });
 });
 
-app.listen(3001, () => {
+const server = app.listen(3001, () => {
     console.info("Now listening on port 3001!")
 });
+
+// Ensure we shut down our server once we exit out
+process.on('SIGTERM', stopServer);
+process.on('SIGINT', stopServer);
