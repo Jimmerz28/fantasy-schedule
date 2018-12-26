0ac5f196a1feaab0c66b1b2f807e516f82906616
James Holby <james.holby@mediaman.de>
2018-12-26 01:34:45 +0100

    Updates

---
+++ precache-manifest.dbc61b0aac495c319fd59fa9b20a91e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a08bf859fc9606fdc7d",
+    "url": "/fantasy-schedule/static/css/main.373efbc0.chunk.css"
+  },
+  {
+    "revision": "4a08bf859fc9606fdc7d",
+    "url": "/fantasy-schedule/static/js/main.4a08bf85.chunk.js"
+  },
+  {
+    "revision": "f1f7a355a41f096f3a56",
+    "url": "/fantasy-schedule/static/css/1.d0526c8a.chunk.css"
+  },
+  {
+    "revision": "f1f7a355a41f096f3a56",
+    "url": "/fantasy-schedule/static/js/1.f1f7a355.chunk.js"
+  },
+  {
+    "revision": "a74720d931cca157848f",
+    "url": "/fantasy-schedule/static/js/runtime~main.a74720d9.js"
+  },
+  {
+    "revision": "826c5ff339791ed5b761ae2c0edc797b",
+    "url": "/fantasy-schedule/index.html"
+  }
+];
