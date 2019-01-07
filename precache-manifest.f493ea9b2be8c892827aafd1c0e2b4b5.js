2fc4f575f8421e29f05f94f329bf339feb708de6
James Holby <james.holby@mediaman.de>
2019-01-07 01:00:59 +0100

    Updates

---
+++ precache-manifest.f493ea9b2be8c892827aafd1c0e2b4b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a74720d931cca157848f",
+    "url": "/fantasy-schedule/static/js/runtime~main.a74720d9.js"
+  },
+  {
+    "revision": "0e751a8708b4f0fd1a9b",
+    "url": "/fantasy-schedule/static/js/main.0e751a87.chunk.js"
+  },
+  {
+    "revision": "5a028658c823ea6c63ae",
+    "url": "/fantasy-schedule/static/js/1.5a028658.chunk.js"
+  },
+  {
+    "revision": "0e751a8708b4f0fd1a9b",
+    "url": "/fantasy-schedule/static/css/main.4327ece1.chunk.css"
+  },
+  {
+    "revision": "5a028658c823ea6c63ae",
+    "url": "/fantasy-schedule/static/css/1.d0526c8a.chunk.css"
+  },
+  {
+    "revision": "831bc00e57aea95d40e395fe1b6e6d4d",
+    "url": "/fantasy-schedule/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -15,7 +15,7 @@ importScripts("/fantasy-schedule/workbox-v3.6.2/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "/fantasy-schedule/workbox-v3.6.2"});
 
 importScripts(
-  "/fantasy-schedule/precache-manifest.817c4efc77f711f6e94c5879dbc59b0a.js"
+  "/fantasy-schedule/precache-manifest.f493ea9b2be8c892827aafd1c0e2b4b5.js"
 );
 
 workbox.clientsClaim();
